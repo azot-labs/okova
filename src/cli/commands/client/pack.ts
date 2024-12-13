defcb4ded0d9c566cf6771039479489a7eb4c155
Vitaly Gashkov <vitalygashkov@vk.com>
2024-12-13 16:49:06 +0500

    fix: improve filename of packed client

--- src/cli/commands/client/pack.ts
+++ src/cli/commands/client/pack.ts
@@ -10,7 +10,10 @@ export const pack = async (
   const client = await importClient(input);
   const ext = format || (output ? extname(output) : 'azot');
   const data = await client.pack(ext as 'wvd' | 'azot' | undefined);
-  const filename = `${client.info.get('company_name')}-${client.info.get('model_name')?.toLowerCase()}`;
+  const filename =
+    `${client.info.get('company_name')}-${client.info.get('model_name')}`
+      .replaceAll(' ', '-')
+      .toLowerCase();
   const outputPath = output || join(process.cwd(), `${filename}.${ext}`);
   await writeFile(outputPath, data);
   console.log(`Client packed: ${outputPath}`);
